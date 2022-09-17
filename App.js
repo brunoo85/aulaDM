cb8b4efc0d1237b905e35c6635ae0cc7e774392f
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2022-09-17 09:53:00 -0300

    alterei no dna (1 vez)

--- App.js
+++ App.js
@@ -14,20 +14,29 @@ export default function App() {
         </View>
         <View style={styles.stories}>
           <View style={styles.story}>
-
+            <Image style={styles.story1} source={require('./assets/imagens/ines.jpg')}/>
+            <Text style={styles.textoStories}>Inês</Text>
           </View>
           <View style={styles.story}>
-
+            <Image style={styles.story1} source={require('./assets/imagens/gretchen.jpg')}/>
+            <Text style={styles.textoStories}>Gretchen</Text>
           </View>
           <View style={styles.story}>
-            
-            </View>
-            <View style={styles.story}>
-            
-            </View>
-            <View style={styles.story}>
-            
-            </View>
+            <Image style={styles.story1} source={require('./assets/imagens/tuluana.jpg')}/>
+            <Text style={styles.textoStories}>Tula</Text>
+          </View>
+          <View style={styles.story}>
+            <Image style={styles.story1} source={require('./assets/imagens/chuu.jpg')}/>
+            <Text style={styles.textoStories}>Chuu</Text>
+          </View>
+          <View style={styles.story}>
+            <Image style={styles.story1} source={require('./assets/imagens/ladygaga.jpg')}/>
+            <Text style={styles.textoStories}>Gaga</Text>
+          </View>
+          <View style={styles.story}>
+            <Image style={styles.story1} source={require('./assets/imagens/jesus1.jpg')}/>
+            <Text style={styles.textoStories}>Inês</Text>
+          </View>
         </View>
         <View style={styles.feed}>
           <View style={styles.post}>
@@ -39,10 +48,42 @@ export default function App() {
               <FontAwesome5 name='ellipsis-h' size={16} color='black'/>
             </View>
             <Image style={styles.postimg} aspectRatio={1.777} source={require('./assets/imagens/propostasChuu.jpeg')}/>
+            <View style={styles.footer}>
+              <FontAwesome5 style={styles.footerIcon}name='heart' size={36} color='black'/>
+              <FontAwesome5 style={styles.footerIcon}name='comment' size={36} color='black'/>
+            </View>
           </View>
-          <View style={styles.footer}>
 
+          <View style={styles.post}>
+            <View style={styles.postHeader}>
+              <View style={styles.postHeaderesquerda}>
+              <Image style={styles.postHeaderimg}source={require('./assets/imagens/ladygaga.jpg')}/>
+              <Text style={styles.nomeDoUsuario}>Laladydy Gagagaga</Text>
+              </View>
+              <FontAwesome5 name='ellipsis-h' size={16} color='black'/>
+            </View>
+            <Image style={styles.postimg} aspectRatio={1.777} source={require('./assets/imagens/vaziaVulgar.jpg')}/>
+            <View style={styles.footer}>
+              <FontAwesome5 style={styles.footerIcon}name='heart' size={36} color='black'/>
+              <FontAwesome5 style={styles.footerIcon}name='comment' size={36} color='black'/>
+            </View>
           </View>
+
+          <View style={styles.post}>
+            <View style={styles.postHeader}>
+              <View style={styles.postHeaderesquerda}>
+              <Image style={styles.postHeaderimg}source={require('./assets/imagens/ines.jpg')}/>
+              <Text style={styles.nomeDoUsuario}>Pantera Selvagem</Text>
+              </View>
+              <FontAwesome5 name='ellipsis-h' size={16} color='black'/>
+            </View>
+            <Image style={styles.postimg} aspectRatio={1.777} source={require('./assets/imagens/inesMeme.jpg')}/>
+            <View style={styles.footer}>
+              <FontAwesome5 style={styles.footerIcon}name='heart' size={36} color='black'/>
+              <FontAwesome5 style={styles.footerIcon}name='comment' size={36} color='black'/>
+            </View>
+          </View>
+
         </View>
     </View>
   );
@@ -58,7 +99,7 @@ const styles = StyleSheet.create({
     flexDirection: 'row',
     padding: 10,
     height: 50,
-    backgroundColor: '#3C91E6',
+    backgroundColor: '#B0E298',
     alignItens: 'center',
     justifyContent: 'space-between',
   },
@@ -78,8 +119,19 @@ const styles = StyleSheet.create({
   story: {
     height:90,
     width:90,
-    backgroundColor: '#3D3B8E',
+    backgroundColor: '#B0E298',
+    alignItems: 'center',
   },
+  story1:{
+    height:70,
+    width:70,
+    //backgroundColor: '#B0E294',
+    borderRadius: 35,
+},
+textoStories:{
+  fontSize:15,
+  color: "black",
+},
   feed: {
     flex: 1,
   },
@@ -113,9 +165,14 @@ const styles = StyleSheet.create({
     height: undefined,
   },
   footer:{
-    height:60,
+    height:45,
     backgroundColor: '#B0E298',
     flexDirection: 'row',
+    alignItems: 'center',
+    
   },
+  footerIcon:{
+    margin: 5,
+  }
 
 });
