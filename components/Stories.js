f742c0dc55d92656ce508a1fbf64d6acfc80444b
Bruno Cesar <83827977+brunoo85@users.noreply.github.com>
2022-10-14 11:51:31 -0300

    Update Stories.js

--- components/Stories.js
+++ components/Stories.js
@@ -6,7 +6,7 @@ export default function Stories() {
 
   useEffect(function(){
     async function getData(){
-      const response = await fetch('https://raw.githubusercontent.com/brunoo85/projeto_1.0/main/listaMenu.json');
+      const response = await fetch('https://raw.githubusercontent.com/brunoo85/aulaDM/main/lista_story.json');
       const storiesServidor = await response.json();
        setStories(storiesServidor)
     }
@@ -16,7 +16,7 @@ export default function Stories() {
   function renderItem({item}){
     return <View style={styles.story}>
     <Image style={styles.story1} source={{uri: item.imgPerfilUri}}/>
-    <Text style={styles.textoStories}>{item.nomeUsuario}</Text>
+    <Text style={styles.textoStories}>{item.texto}</Text>
   </View>
   }
       return (
